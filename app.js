473f42d31963e1db8079129ac4b820c2be38c205
Onionie <gutierrez.onionie@gmail.com>
2021-06-15 15:39:41 -0700

    GeoCode

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const request = require('request'); //to make http calls
 const apiKey = process.env.API_KEY; //Hidden API key access
 
 
-
-
+// const url = "http://api.weatherstack.com/current?access_key=" + apiKey + "&query=37.8267,-122.4233&units=f";
+//
 // request({url: url, json: true}, function(error, response){
 //   const data = response.body;
 //   if(error){ // low level error
@@ -22,21 +22,38 @@ const apiKey = process.env.API_KEY; //Hidden API key access
 //
 // });
 
-const location = (address, callback) => {
-  const url = "http://api.weatherstack.com/current?access_key=" + apiKey + "&query=37.8267,-122.4233&units=f";
+// const location = (address, callback) => {
+//
+//
+//   //json:true parses the JSON.pars(response.body) to object
+//   request( {url: url, json: true}, (error, response) => {
+//     if(error){
+//       callback("Unable to connect", undefined);
+//     }else if(response.body.features.length === 0){
+//       callback("Unable to find location. Try another search.", undefined);
+//     }
+//
+//   });
+// }
+//
+// location("asdasd", (error,data)=>{
+//   console.log("Error", error);
+//   console.log("Data", data);
+// });
 
-  //json:true parses the JSON.pars(response.body) to object
-  request( {url: url, json: true}, (error, response) => {
-    if(error){
-      callback("Unable to connect", undefined);
-    }else if(response.body.features.length === 0){
-      callback("Unable to find location. Try another search.", undefined);
-    }
-
-  });
-}
-
-location("asdasd", (error,data)=>{
-  console.log("Error", error);
-  console.log("Data", data);
+//Geocode API
+// Request for geocoding to get our latitude and longitude
+const geoCodingUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoib25pb25pZSIsImEiOiJja3B5amRldTkwMHZlMzFtcGw4NmVxN3Z0In0.UjVn1bfDC8FkP2JQToTr_Q";
+  request({url: geoCodingUrl, json: true}, (error, response)=>{
+  if(error){
+    console.log("Network Problem");
+  }
+  else if(response.body.features.length === 0){
+    console.log("No Matching Result.");
+  }
+  else{
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+  }
 });
