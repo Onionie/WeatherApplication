bd275c70d9008db7413c1c6e50cfaff435633b97
Onionie <gutierrez.onionie@gmail.com>
2021-06-15 14:02:24 -0700

    Different API

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Weather Application
-A simple weather application that fetches the current weather of a location that the user is looking for. This Application uses HTML, CSS,Node.js, and OpenWeatherMap API, which creates a pleasant UI/UX for the users.
+A simple weather application that fetches the current weather of a location that the user is looking for. This Application uses HTML, CSS,Node.js, and WeatherStack API, which creates a pleasant UI/UX for the users.
 
 # Tools Used
 - HTML
 - CSS
 - Node.js
-- OpenWeatherMap API
+- WeatherStack API
 - NPM(packages)
 
 # Screenshots
--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 //jshint esversion:6
 
 require('dotenv').config(); // for my api
-const request = require('request');
+const request = require('request'); //to make http calls
 
 const apiKey = process.env.API_KEY; //Hidden API key access
-const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=" + apiKey + "&units=metric";
 
 
 
-request({url: url, json: true}, function(error, response){
-  const data = response.body;
-  if(error){ // low level error
-    console.log("Unable to connect to the Weather API");
-  }
-  else if(response.body.error){ // error in the data
-    console.log("Unable to find location");
-  }
-  else{
-    console.log("It is currently " + data.main.temp + " degrees out. And currently " + data.weather[0].description );
-  }
 
+// request({url: url, json: true}, function(error, response){
+//   const data = response.body;
+//   if(error){ // low level error
+//     console.log("Unable to connect to the Weather API");
+//   }
+//   else if(response.body.error){ // error in the data
+//     console.log("Unable to find location");
+//   }
+//   else{
+//     console.log("It is currently " + data.main.temp + " degrees out. And currently " + data.weather[0].description );
+//   }
+//
+// });
 
+const location = (address, callback) => {
+  const url = "http://api.weatherstack.com/current?access_key=" + apiKey + "&query=37.8267,-122.4233&units=f";
+
+  //json:true parses the JSON.pars(response.body) to object
+  request( {url: url, json: true}, (error, response) => {
+    if(error){
+      callback("Unable to connect", undefined);
+    }else if(response.body.features.length === 0){
+      callback("Unable to find location. Try another search.", undefined);
+    }
+
+  });
+}
+
+location("asdasd", (error,data)=>{
+  console.log("Error", error);
+  console.log("Data", data);
 });
